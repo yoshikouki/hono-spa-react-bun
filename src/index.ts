19a995ad7d3d04d30f91fe4d4d4792d8c94c7304
yoshikouki <yoshikouki@gmail.com>
2024-03-01 00:14:08 +0900

    Watch file changes for client

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
 import { build, serve } from "bun";
+import { watch } from "fs";
 import app from "./server";
 
 const port = process.env.PORT || 3000;
 
 console.log(`Listening on http://localhost:${port}`);
 
-if (process.env.NODE_ENV !== "production") {
+const buildClient = () => {
   build({
     entrypoints: ["src/client.tsx"],
     outdir: "public/static",
     minify: true,
     sourcemap: "external",
-  });
+  })
   console.log("Builded!");
+};
+
+if (process.env.NODE_ENV !== "production") {
+  await buildClient();
+  watch("src/client.tsx", {}, buildClient);
 }
 
 serve({
